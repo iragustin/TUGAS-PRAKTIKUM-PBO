4112ed59525ba9daebe9beac297fc4064db6bfaa
iragustin <101377967+iragustin@users.noreply.github.com>
2022-03-11 14:52:58 +0700

    Add files via upload

---
+++ demo/src/test/java/id/ac/uin/ilham/rizky/agustin/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.uin.ilham.rizky.agustin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
